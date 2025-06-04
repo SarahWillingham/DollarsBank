b1be6aca2709eb7e5f01cf156f14423f98696c55
SarahWillingham <102173982+SarahWillingham@users.noreply.github.com>
2025-06-04 18:09:09 -0400

    Update Transaction.java

--- src/main/java/com/cognixia/jump/model/Transaction.java
+++ src/main/java/com/cognixia/jump/model/Transaction.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main.java.com.cognixia.jump.model;
 
-/**
- *
- * @author sarah
- */
 public class Transaction {
     
     private int amount;
